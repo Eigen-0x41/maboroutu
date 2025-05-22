85c2580067f795722697bc91e3cf40f716f9d2a1
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2025-05-22 20:06:41 +0900

    update

--- include/exexpected.hpp
+++ include/exexpected.hpp
@@ -11,6 +11,6 @@ using expected = typename std::expected<T, typename std::string>;
 template <class T>
 typename expected<T>::unexpected_type
 unexpectedConv(expected<T> const &Expected) {
-  return expected<T>::unexpected_type(Expected.error());
+  return typename expected<T>::unexpected_type(Expected.error());
 }
 } // namespace maboroutu
