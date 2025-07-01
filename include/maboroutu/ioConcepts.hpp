ee60863ef34440c62b6c9975db4810b022c26d86
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2025-07-01 15:51:02 +0900

    fix

--- include/maboroutu/ioConcepts.hpp
+++ include/maboroutu/ioConcepts.hpp
@@ -4,9 +4,9 @@
 #include <concepts>
 #include <type_traits>
 namespace maboroutu {
-template <class T, class ValueT>
+template <class T, class ValueT, class... ArgsT>
 concept IOConcepts = requires(T &IO, ValueT Value) {
-  requires std::is_constructible_v<T, binary &>;
+  requires std::is_constructible_v<T, binary &, ArgsT...>;
   requires std::is_constructible_v<T, binary &, size_t>;
   { Value = IO };
   { IO = Value };
