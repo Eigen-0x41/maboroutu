da4eb3d60def1e682471b8387b287c8c6e783504
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2025-09-25 16:26:53 +0900

    add new VRef.

--- include/maboroutu/maboroutuDef.hpp
+++ include/maboroutu/maboroutuDef.hpp
@@ -28,62 +28,78 @@ template <class T> retErr convRetErr(ret<T> const &Ret) {
   return retErr{Ret.error()};
 }
 
-// /**
-//  * @brief classによる参照型の実装。
-//  * CV修飾、pointer型を型Tに代入することはできません。
-//  *
-//  * @tparam T [TODO:tparam]
-//  * @return [TODO:return]
-//  */
-// template <class T> class VRef {
-// private:
-// protected:
-// public:
-//   using this_type = VRef;
-//
-//   using value_type = T;
-//   static_assert(!std::is_const_v<value_type>, "Is not const.");
-//   static_assert(!std::is_volatile_v<value_type>, "Is not volatile.");
-//   static_assert(!std::is_pointer_v<value_type>, "Is not pointer.");
-//
-//   using pointer = value_type *;
-//   using const_pointer = value_type const *;
-//   using reference = value_type &;
-//   using const_reference = value_type const &;
-//
-// private:
-//   pointer Value;
-//
-// protected:
-// public:
-//   VRef() = delete;
-//   VRef(this_type const &This) : Value(This.Value) {}
-//   VRef(this_type &&This) : Value(This.Value) {}
-//   VRef(reference Value) : Value(&Value) {}
-//   ~VRef() = default;
-//   this_type &operator=(this_type const &This) {
-//     *Value = *This.Value;
-//     return *this;
-//   }
-//   this_type &operator=(this_type &&This) {
-//     Value = This.Value;
-//     return *this;
-//   };
-//   this_type &operator=(const_reference V) noexcept {
-//     *Value = V;
-//     return *this;
-//   }
-//   operator reference() noexcept { return *Value; }
-//   decltype(auto) operator*() noexcept { return **Value; }
-//   pointer operator->() noexcept { return Value; }
-//
-//   friend bool operator==(this_type const &Lhs, this_type const &Rhs) noexcept {
-//     return *Lhs.Value == *Rhs.Value;
-//   }
-//
-//   friend bool operator==(this_type const &Lhs, const_reference Rhs) noexcept {
-//     return *Lhs.Value == Rhs;
-//   }
-// };
+/**
+ * @brief classによる参照型の実装。
+ * CV修飾、pointer型を型Tに代入することはできません。
+ *
+ * @tparam T [TODO:tparam]
+ * @return [TODO:return]
+ */
+template <class T> class VRef {
+private:
+protected:
+public:
+  using value_type = T;
+
+private:
+  T *Value;
+
+protected:
+public:
+  VRef() = delete;
+  VRef(VRef const &This) = delete;
+  VRef(VRef &&This) = delete;
+  explicit VRef(std::reference_wrapper<value_type> Value)
+      : Value(&Value.get()) {}
+  ~VRef() = default;
+
+  template <class TLoc> VRef &operator=(TLoc const &Value) {
+    *Value = Value;
+    return *this;
+  }
+
+  template <class TLoc> VRef &operator=(VRef<TLoc> This) {
+    *Value = *This.Value;
+    return *this;
+  }
+
+  VRef &operator=(VRef &&This) {
+    Value = This.Value;
+    return *this;
+  };
+  VRef &operator=(T const &V) noexcept {
+    *Value = V;
+    return *this;
+  }
+
+  T &get() const noexcept { return *Value; }
+
+  operator T &() const noexcept { return *Value; }
+
+  decltype(auto) operator*() const noexcept { return **Value; }
+
+  T *operator->() const noexcept { return Value; }
+
+  template <class... ArgsT>
+  constexpr std::invoke_result_t<T &, ArgsT...>
+  operator()(ArgsT &&...Args) const {
+    return Value(std::forward<ArgsT>(Args)...);
+  }
+
+  friend bool operator==(VRef const &Lhs, VRef const &Rhs) noexcept {
+    return Lhs.Value == Rhs.Value;
+  }
+
+  friend bool operator==(VRef const &Lhs, T const &Rhs) noexcept {
+    return *Lhs.Value == Rhs;
+  }
+};
 
 } // namespace maboroutu
+
+namespace std {
+template <class T>
+constexpr reference_wrapper<T> ref(maboroutu::VRef<T> &Value) noexcept {
+  return ref(Value.get());
+}
+} // namespace std
