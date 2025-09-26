ad100291b69841c35130abcc5bb4b3dfb091be91
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2025-09-27 00:27:53 +0900

    test update.

--- portfile.cmake
+++ portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Eigen-0x41/maboroutu
-    REF "${VERSION}" # 0.0.0.1
+    REF "${VERSION}" # 0.0.0.2
     SHA512 c80eb4bcf18a7923be6b23d1632927912be9af7bc28c0c27c2617c6a80dfa81b3b9a7af20b6c9c18cc94b7490559cc5ee4bfb5de3c8c63e232ef7090d1cd747a
     HEAD_REF maboroutu
 )
@@ -16,7 +16,7 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME maboroutu)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
