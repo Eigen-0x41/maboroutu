04656ba2783db34def367c740803ef119f85f744
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2025-09-27 20:38:57 +0900

    update

--- portfile.cmake
+++ portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Eigen-0x41/maboroutu
-    REF "${VERSION}" # 0.0.0.14
-    SHA512 02f1dcec0587c6f7c0f5637d6a3224b87ef068315b01f387bb6bffe3ca9ae2ab9fff11bd84a6c7fa1980de80f4c4516a5fc79e30f65bffefaf0d0b462ae637cd
+    REF "${VERSION}" # 0.0.0.15
+    SHA512 0
     HEAD_REF maboroutu
 )
 
--- test/maboroutu/BitAccess.cpp
+++ test/maboroutu/BitAccess.cpp
@@ -10,12 +10,12 @@ BOOST_AUTO_TEST_CASE(BitAccess) {
   using field_type = uint16_t;
 
   using bitAccess_type = maboroutu::BitAccess<field_type>;
-  using access1 = bitAccess_type::main_key_t<0, 4>;
-  using access2 = bitAccess_type::key_t<access1, 4>::value_type;
-  using access3 = bitAccess_type::main_key_t<0, 8>::value_type;
+  using access1 = typename bitAccess_type::main_key_t<0, 4>;
+  using access2 = bitAccess_type::key_t<access1, 4>;
+  using access3 = bitAccess_type::main_key_t<0, 8>;
 
-  using access4 = bitAccess_type::main_key_t<0, 16>::value_type;  // allow
-  using access5 = bitAccess_type::key_t<access1, 12>::value_type; // allow
+  using access4 = bitAccess_type::main_key_t<0, 16>;  // allow
+  using access5 = bitAccess_type::key_t<access1, 12>; // allow
 
   // using access6 = bitAccess_type::KeyMaker<0, 17>::value_type; // compile
   // error! using access7 =
@@ -30,22 +30,22 @@ BOOST_AUTO_TEST_CASE(BitAccess) {
   bitAccess_type Test;
 
   field_type PrintVal = Test(access1());
-  std::println(std::cout, "{:b}", PrintVal);
+  std::println(std::cout, "0:{:b}", PrintVal);
   Test(access1()) = 0b1001;
   PrintVal = Test(access1());
-  std::println(std::cout, "{:b}", PrintVal);
+  std::println(std::cout, "1:{:b}", PrintVal);
 
   PrintVal = Test(access2());
-  std::println(std::cout, "{:b}", PrintVal);
+  std::println(std::cout, "2:{:b}", PrintVal);
   Test(access2()) = 0b11111001;
   PrintVal = Test(access2());
-  std::println(std::cout, "{:b}", PrintVal);
+  std::println(std::cout, "3:{:b}", PrintVal);
 
   PrintVal = Test(access3());
-  std::println(std::cout, "{:b}", PrintVal);
+  std::println(std::cout, "4:{:b}", PrintVal);
   Test(access3()) = 0b1110111011;
   PrintVal = Test(access3());
-  std::println(std::cout, "{:b}", PrintVal);
+  std::println(std::cout, "5:{:b}", PrintVal);
 
-  std::println(std::cout, "{:b}", *Test);
+  std::println(std::cout, "6:{:b}", Test.value());
 }
