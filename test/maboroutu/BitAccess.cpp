83d555894655dc92d9efc9ec8a2f6e2208cf9ea1
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2025-09-27 13:59:03 +0900

    bitAcess.hpp: delete KeyMaker. using key_t.

--- test/maboroutu/BitAccess.cpp
+++ test/maboroutu/BitAccess.cpp
@@ -10,13 +10,12 @@ BOOST_AUTO_TEST_CASE(BitAccess) {
   using field_type = uint16_t;
 
   using bitAccess_type = maboroutu::BitAccess<field_type>;
-  using access1 = bitAccess_type::KeyMaker<0, 4>::value_type;
-  using access2 = bitAccess_type::DependedKeyMaker<access1, 4>::value_type;
-  using access3 = bitAccess_type::KeyMaker<0, 8>::value_type;
+  using access1 = bitAccess_type::main_key_t<0, 4>;
+  using access2 = bitAccess_type::key_t<access1, 4>::value_type;
+  using access3 = bitAccess_type::main_key_t<0, 8>::value_type;
 
-  using access4 = bitAccess_type::KeyMaker<0, 16>::value_type; // allow
-  using access5 =
-      bitAccess_type::DependedKeyMaker<access1, 12>::value_type; // allow
+  using access4 = bitAccess_type::main_key_t<0, 16>::value_type;  // allow
+  using access5 = bitAccess_type::key_t<access1, 12>::value_type; // allow
 
   // using access6 = bitAccess_type::KeyMaker<0, 17>::value_type; // compile
   // error! using access7 =
