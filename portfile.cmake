c30b5e593e0572e63a965d3c0d34ba4594aeb52b
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2025-09-28 06:29:02 +0900

    update

--- portfile.cmake
+++ portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Eigen-0x41/maboroutu
-    REF "${VERSION}" # 0.0.0.15
-    SHA512 3eb1b089e64514aa8a5b4e4ff1cd1e85703081a27f25655e950aa49784780f608d4c43f24be764392aeea813ac692b69bc998d3c4d31a3cb105c2f05d26c54f6
+    REF "${VERSION}" # 0.0.0.16
+    SHA512 0
     HEAD_REF maboroutu
 )
 
