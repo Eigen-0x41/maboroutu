ed369fa933086375c15a095c7b40bb4374fdbde7
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2025-12-23 02:07:48 +0900

    エラー戦略の変更

--- portfile.cmake
+++ portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Eigen-0x41/maboroutu
-    REF "${VERSION}" # 0.0.1.3
-    SHA512 92fb7c7d84b1b0aecaf86987a855e8ad294f7a5a1a186399ea30ee5d9f5aa991f76ca6aac52cdc7bab744875c6f7766a69b9a3d9914bf1667e4f9612525feeac
+    REF "${VERSION}" # 0.0.1.4
+    SHA512 0
     HEAD_REF maboroutu
 )
 
