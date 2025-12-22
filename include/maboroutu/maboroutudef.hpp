a173d0d09358cdcfae82b78a0ac807f678bb004d
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2025-12-23 02:07:48 +0900

    add roslyn

--- include/maboroutu/maboroutudef.hpp
+++ include/maboroutu/maboroutudef.hpp
@@ -1,31 +1,26 @@
 #pragma once
 
-#include "maboroutu/error.hpp"
+#include <concepts>
 #include <cstddef>
+#include <exception>
 #include <expected>
 #include <vector>
 namespace maboroutu {
 using byte_t = typename std::byte;
 using binary_t = typename std::vector<byte_t>;
 
-template <class Ty = void, exception_concepts Err = exception>
-using ret_type = typename std::expected<Ty, Err>;
-
-template <class T> struct make_ret_error_categoly {
-  using type = typename T::error_type::categoly_type;
-};
 template <class T>
-using make_ret_error_categoly_t = make_ret_error_categoly<T>::type;
-template <class T> struct make_ret_error_descript {
-  using type = typename T::error_type::descript_type;
-};
-template <class T>
-using make_ret_error_descript_t = make_ret_error_descript<T>::type;
+using ret_type = typename std::expected<T, std::exception const *>;
 
-template <class ExpectedT, class... ArgsT>
-auto make_ret_error(ArgsT &&...args) -> ExpectedT::unexprcted_type {
-  return ExpectedT::unexpected_type(
-      ExpectedT::error_type(std::forward<ArgsT>(args)...));
+template <class T, class... Args>
+  requires std::constructible_from<T, Args...> &&
+           std::derived_from<T, std::exception>
+auto make_exception_error(Args &&...args) -> ret_type<void>::unexpected_type {
+  static T holder(std::forward<Args>(args)...);
+  return ret_type<void>::unexpected_type(std::in_place, &holder);
 }
-
+template <class T>
+constexpr auto err_cast(T &value) -> ret_type<void>::unexpected_type {
+  return ret_type<void>::unexpected_type(value.error());
+}
 } // namespace maboroutu
